2683dabb77765ad967440aebcac93faf1518d652
vagnerlucas <vagnerlucas@hotmail.com>
2016-10-21 21:34:14 +0100

    dialogService

---
+++ src/services/dialogService.js
@@ -0,0 +1,29 @@
+(function () {
+
+    function dialogService($mdDialog) {
+        let vm = this
+
+        vm.openConfirmDialog = ($event, title, msg, yes, no) => {
+            yes = yes || 'Yes'
+            no = no || 'No'
+            const confirm = $mdDialog.confirm().title(title).textContent(msg).ok(yes).cancel(no).targetEvent($event)
+            return $mdDialog.show(confirm)
+        }
+
+        vm.showMessage = (title, msg, ok) => {
+            title = title || 'Alert'
+            ok = ok || 'Ok'
+            const alert = $mdDialog.alert().title(title).textContent(msg).ok(ok)
+            return $mdDialog.show(alert)
+        }
+    }
+
+    require(['app'], function (app) {
+        app.service('dialogService', dialogService)
+    })
+
+    define(['app', 'services/DBService'], function () {
+        return dialogService
+    })
+
+})()
